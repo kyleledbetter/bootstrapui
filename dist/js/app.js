5834f16929e29569fc5d9acd7c3bcb31db13e133
kyleledbetter <kyle@kyleledbetter.com>
2014-06-17 16:41:59 -0500

    Adding tooltip js

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,5 +5,6 @@ $(document).ready(function () {
   $('[data-toggle="aside"]').click(function () {
     $('.aside').toggle('slow');
   });
+  $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover({ trigger: "hover" });
 });
